bdbbb7f0cc080cbd94742eed8c59b971e898c3de
igor2533 <igor25_33@mail.ru>
2020-02-17 01:04:31 +0300

    third version

---
+++ models/Users.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "users".
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $value
+ */
+class Users extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'users';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'value'], 'required'],
+            [['name'], 'string'],
+            [['value'], 'integer'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'value' => 'Value',
+        ];
+    }
+}
---
+++ views/site/retr.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+
+
+?>
+
+
+PRIVET MEDVED
